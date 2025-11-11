2b50b42d2b0ba4590d603c918c1f14ce5134eeca
DikaSiNpc <dikagiowanda33@gmail.com>
2025-11-11 11:51:34 +0700

    PrakAlproPekan7_2511533025

---
+++ 2025_prakalpro_c_2511533025/src/pekan7_2511533025/PanggilMahasiwa_2511533025.java
@@ -0,0 +1,15 @@
+package pekan7_2511533025;
+
+public class PanggilMahasiwa_2511533025 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533025 a= new Mahasiswa_2511533025();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+
+	}
+
+}
---
+++ 2025_prakalpro_c_2511533025/src/pekan7_2511533025/String1_2511533025.java
@@ -0,0 +1,14 @@
+package pekan7_2511533025;
+
+public class String1_2511533025 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("panjang salam adalah: " + salam.length());
+		System.out.println(salam.toUpperCase());  // Outputs "ASSALAMUALAIKUM"
+		System.out.println(salam.toUpperCase());  // Outputs "assalamualaikum"
+		System.out.println(salam.indexOf("salam"));  // Outputs 2
+
+	}
+
+}
